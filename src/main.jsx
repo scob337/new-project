0ec8ee0a55830f60e4d2fd4319b7f6169edfb184
Abd Eltawab Shaaban <105607290+scob337@users.noreply.github.com>
2025-03-15 21:42:04 +0200

    add loading Animation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import MainLayOut from './Layouts/MainLayOut.jsx';
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MainLayOut from "./Layouts/MainLayOut.jsx";
+import { Helmet } from "react-helmet";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Cadonizer</title>
+      <meta
+        name="description"
+        content="A modern and elegant interior design studio."
+      />
+      <meta
+        name="keywords"
+        content="interior design, modern design, minimalist design, minimalist living"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Helmet>
+
     <MainLayOut />
   </StrictMode>
 );
